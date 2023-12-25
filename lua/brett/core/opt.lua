23f18f56b9d2442f5e07d05498b5b9677c31409c
Brett <brett@brettfreeman.io>
2023-12-25 11:06:54 -0500

    Modifying clipboard to be unnamedplus

--- lua/brett/core/opt.lua
+++ lua/brett/core/opt.lua
@@ -34,3 +34,5 @@ vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGai
     command = "if mode() != 'c' | checktime | endif",
     pattern = { "*" },
 })
+
+vim.opt.clipboard = "unnamedplus"
