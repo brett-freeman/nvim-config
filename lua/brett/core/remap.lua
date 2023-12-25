8aecb5776385c268015bf50515faef6954027fe2
Brett <brett@brettfreeman.io>
2023-12-25 14:59:05 -0500

    Remove custom copy/paste keybinds.

--- lua/brett/core/remap.lua
+++ lua/brett/core/remap.lua
@@ -3,6 +3,3 @@ vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
-
-vim.keymap.set('n', 'y', '"*y')
-vim.keymap.set('n', 'p', '"*p')
