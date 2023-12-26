15541db235b862be5b6bccdf2b579cdb7706ac35
= <=>
2023-12-26 12:13:29 -0500

    Messing with tree-sitter-textobjects

--- lua/brett/plugins/treesitter.lua
+++ lua/brett/plugins/treesitter.lua
@@ -1,25 +1,52 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    config = function()
-        local configs = require('nvim-treesitter.configs')
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        config = function()
+            local configs = require('nvim-treesitter.configs')
 
-        configs.setup({
-            ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'python', 'javascript', 'typescript', 'html' },
-            auto_install = true,
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
+            configs.setup({
+                ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'python', 'javascript', 'typescript', 'html' },
+                auto_install = true,
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
 
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = 'gnn',
-                    node_incremental = 'grn',
-                    scope_incremental = 'grc',
-                    node_decremental = 'grm'
-                }
-            },
-        })
-    end
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = '<leader>ss',
+                        node_incremental = '<leader>si',
+                        scope_incremental = '<leader>sc',
+                        node_decremental = '<leader>sd',
+                    },
+                },
+
+                textobjects = {
+                    select = {
+                        enable = true,
+
+                        lookahead = true,
+
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                            ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                        },
+                        selection_modes = {
+                            ['@parameter.outer'] = 'v', -- charwise
+                            ['@function.outer'] = 'V',  -- linewise
+                            ['@class.outer'] = '<c-v>', -- blockwise
+                        },
+                        include_surrounding_whitespace = true,
+                    },
+                },
+            })
+        end
+    },
+    {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+    },
 }
