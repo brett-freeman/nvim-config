ec7f3eb3a8a1536a6485117b1000d293252070fe
= <=>
2024-01-05 01:42:13 -0500

    Removing the annoying scope underlining in indent lines plugin.

--- lua/brett/plugins/ibl.lua
+++ lua/brett/plugins/ibl.lua
@@ -2,6 +2,17 @@ return {
     'lukas-reineke/indent-blankline.nvim', main = 'ibl',
     opts = {},
     config = function()
-        require('ibl').setup()
+        require('ibl').setup({
+            indent = {
+                char = '│',
+            },
+            scope = {
+                show_start = false,
+                show_end = false,
+            }
+        })
+        local hooks = require('ibl.hooks')
+        hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+        hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
     end
 }
